1d8696279886019fb552aa2f57afdaa8b2572aae
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-30 15:23:47 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -28,7 +28,7 @@ class Song < ActiveRecord::Base
    noted = Note.new
    noted.content = content
    noted.save
-   self.notes << noted.content
+   self.notes << content
  end
 
  def note_contents
